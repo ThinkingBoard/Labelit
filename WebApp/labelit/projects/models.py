d8b61f2a1c47a735a45be9a65faa43f12316f63a
Andy Daniel Martinez <mhdaniel24@knights.ucf.edu>
2019-02-18 15:20:52 -0500

    Labelit-8 - Minor lines change

--- WebApp/labelit/projects/models.py
+++ WebApp/labelit/projects/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
 
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE)
+       User, on_delete=models.CASCADE)
 
     project_name = models.CharField(max_length=200)
 
--- WebApp/labelit/projects/views.py
+++ WebApp/labelit/projects/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    #request.use
     return render(request, 'projects/index.html')
     #return HttpResponse("Hello, world. You're at the polls index.")
